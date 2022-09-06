e71269ddd36d7247473f401082a200ece0316b90
Katarzyna Bedlińśka <kbedlinska@gmail.com>
2022-09-06 16:42:44 +0200

    fix folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import "./App.css";
 import Nav from "./components/Nav";
 import Header from "./components/Header";
 import Employees from "./components/Employees";
 import Services from "./components/Services";
 import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
